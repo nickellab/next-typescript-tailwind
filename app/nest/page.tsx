b4388a426b4e0ec35dd5f250ef6234ea8625cfa5
ryosuke mori <ryosuke.mori@nickellab.net>
2023-07-26 23:31:45 +0900

    add metadata

--- app/nest/page.tsx
+++ app/nest/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from 'next'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import Button from '~/components/Button'
 import PostList from '~/features/post/PostList'
 import UserList from '~/features/user/UserList'
+
+export const metadata: Metadata = {
+  title: 'Nest page',
+  description: 'Nest depth page',
+}
 
 const NestPage = () => {
   return (
